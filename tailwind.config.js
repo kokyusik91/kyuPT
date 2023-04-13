72925581df8e0c12b684e52c9a317d259c5faed2
kks91 <kokyusik91@naver.com>
2023-04-13 21:21:19 +0900

    ✨ tailwind.config.js 에서 커스텀 프로퍼티 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const { fontFamily } = require('tailwindcss/defaultTheme')
+// const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -7,36 +8,25 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: ['class', '[data-mode="dark"]'],
+
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        title: `2rem;`,
+        paragraph: `1rem;`,
+      },
+      fontWeight: {
+        custom: '400',
+      },
       colors: {
-        tp: {
-          blue100: '#ECF6FF',
-          blue101: '#F2FBFE',
-          blue200: '#2D9CDB',
-          blue300: '#2F80ED',
-          blue400: '#0E4E97',
-          gray100: '#E8EBED',
-          gray200: '#C9CDD2',
-          gray300: '#9FA4AA',
-          gray400: '#72787F',
-          gray500: '#464C53',
-          gray600: '#26282B',
-          green100: '#EEFDFA',
-          green200: '#00AF89',
-          green300: '#14866D',
-          red100: '#FEE7E6',
-          red200: '#EB5757',
-          red300: '#B32424',
-          white100: '#FFFFFF',
-          white200: '#F7F8F9',
-          yellow100: '#FEF6E7',
-          yellow200: '#FFCC33',
-          yellow300: '#F2994A',
+        cs: {
+          mainblack: '#2F3E39',
+          skyblue: '#98D0EF',
         },
       },
       fontFamily: {
