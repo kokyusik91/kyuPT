5682c4492dd570fb540504574f498cf741ccdf45
kks91 <kokyusik91@naver.com>
2023-04-14 10:55:01 +0900

    🚧 아이콘 파일 따로 생성하여 관리

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,7 +1,11 @@
-import arrow_left_big from '../../../public/icons/arrow_left_big.svg'
+import arrow_left_big from '@icons/arrow_left_big.svg'
+import plus_big from '@icons/plus_big.svg'
+import logo from '@icons/logo.svg'
 
 const icons = {
   arrow_left_big,
+  plus_big,
+  logo,
 }
 
 export default icons
