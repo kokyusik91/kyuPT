3824ef7248757c6851ae7665dda13aaffe0092ee
kks91 <kokyusik91@naver.com>
2023-04-14 10:53:52 +0900

    🚧 메인 로그인 페이지 개발중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,126 +1,71 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Button from '@components/Button'
-import newJeans from '@images/newJeans.png'
-
-const inter = Inter({ subsets: ['latin'] })
+import Button from '@components/common/Button'
+import ScaffoldPage from '@components/layout/ScaffoldPage'
+import AppHeader from '@components/AppHeader'
+import Icon from '@components/Icon/Icon'
+import Input from '@components/common/Input'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const [value, setValue] = useState('')
+  const rotuer = useRouter()
+
+  // 유효성 체크 검사
+  const validCheck = (value: string) => {
+    if (value.trim().length === 0) return 'invalid'
+  }
+
+  const handleClickLogin = () => {
+    // TODO : API key 검증
+    // 메인 페이지로 replace
+    rotuer.replace('/home')
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <ScaffoldPage
+      appHeader={
+        <AppHeader
+          leftSideComponent={
+            <Image src="/images/logo.png" alt="이미지로고" width={124} height={20} />
+          }
+          rightSideComponent={<Icon icon="plus_big" />}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`font-sans ${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-        <Button>버튼</Button>
-        <Image src="/images/newJeans.png" alt="뉴진스 이미지" width={120} height={120} />
+      }
+    >
+      <div className="m-auto text-center mt-40 mb-20">
         <Image
-          src="https://img.gigglehd.com/gg/files/attach/images/158/803/973/013/8074ee59165411ba11b827949f3cd9bc.png"
-          alt="개발 진스"
+          src="/images/gpt-logo.png"
+          alt="gpt logo"
           width={120}
           height={120}
+          style={{ display: 'inline-block' }}
         />
       </div>
-    </main>
+      <div className="w-full px-4">
+        <Input
+          id="API KEY"
+          value={value}
+          setValue={setValue}
+          inputProps={{
+            placeholder: 'API KEY를 입력해주세요',
+          }}
+        />
+      </div>
+      <div className="absolute w-full px-4 bottom-10">
+        <div className="mb-3">
+          <Button
+            txtColor="text-cs-mainblack"
+            backgroundColor="bg-cs-skyblue"
+            handleClickButton={handleClickLogin}
+          >
+            로그인
+          </Button>
+        </div>
+        <p className="text-cs-mainblack text-center underline underline-offset-3">
+          Key 발급받는 방법
+        </p>
+      </div>
+    </ScaffoldPage>
   )
 }
