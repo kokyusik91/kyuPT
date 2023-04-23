0dc217b58a2204b8c230287c5661cedff0a01d01
kks91 <kokyusik91@naver.com>
2023-04-23 22:36:04 +0900

    ✨ GPT API 추가

--- src/util/service.ts
+++ src/util/service.ts
@@ -1,8 +1,13 @@
 import { fetchInstance } from './fetchInstance'
+import { Message } from './type'
 
 const service = {
   fetchLogin: async (apikey: string, callBackFunc: () => void) => {
     return await fetchInstance.post('/api/login', { apikey }, callBackFunc)
+  },
+
+  chatWithGpt: async (messageList: Message[]) => {
+    return await fetchInstance.post('/api/chat', { messageList })
   },
 }
 
