ecce5c44a0883aca303bb48dc7213bb4b1fe44a8
kks91 <kokyusik91@naver.com>
2023-04-23 22:36:25 +0900

    ✨ 유틸 함수들 따로 관리

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,3 +3,35 @@ export const validCheck = (value: string) => {
   if (value.trim().length === 0) return false
   else return true
 }
+
+export const delayFunc = (time: number) => {
+  return new Promise(() => {
+    setTimeout(() => {}, time)
+  })
+}
+
+export function wait(ms: number) {
+  return new Promise((res) => setTimeout(res, ms))
+}
+
+export const transFormToDayFormat = (date: Date) => {
+  if (date) {
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    const dateString = `${year}-${month}-${day}`
+    return dateString
+  }
+
+  return '0000-00-00'
+}
+
+export const transformToTime = (date: Date) => {
+  if (date) {
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    return `${hours}:${minutes}`
+  }
+
+  return '00:00'
+}
